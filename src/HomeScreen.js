23479f90bbc6e8b1b3c7d6cf237b0fe5d3ec9dc1
Vllaganti <NagaAishwarya.Vllaganti@allscripts.com>
2021-08-12 18:23:37 +0530

    home screen

---
+++ src/HomeScreen.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import './App.css';
+
+export class HomeScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+       
+        }      
+    }
+    render() {
+        return (
+          <div>                 
+            
+          </div>
+          
+        );
+      }
+  
+}
